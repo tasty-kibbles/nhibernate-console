416fae739f8581893e33652473c3ad42a5b56f7c
Steve Jones <sjwilliamson111@gmail.com>
2020-01-17 12:04:49 +0000

    create new repository

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleNHibernate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world");
+        }
+    }
+}
